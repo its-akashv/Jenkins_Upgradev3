c62c0e15376097c3816b0874b7a80516a005cf55
its-akashv <104986630+its-akashv@users.noreply.github.com>
2023-12-28 12:42:52 +0530

    Update Greeter.java

--- maven-samples/single-module/src/main/java/com/example/Greeter.java
+++ maven-samples/single-module/src/main/java/com/example/Greeter.java
@@ -21,7 +21,7 @@ public class Greeter {
   
   public static void main(String args[]){
     
-	  System.out.println("Hello my friend!!!");
+	  System.out.println("Hello my friend...!!!");
 	
   }
 	
